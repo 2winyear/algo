12243c859dd4f5d89d77d561d908b02d541e0c5c
2winyear <lsy2246@naver.com>
2022-12-19 22:43:34 +0900

    test

---
+++ B1009.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class B1009 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count;
+        int[] num1 = new int[1000000];
+        int[] num2 = new int[1000000];
+        count = sc.nextInt();
+        for (int i = 0; i < count; i++) {
+            num1[i] = sc.nextInt();
+            num2[i] = sc.nextInt();
+        }
+        for (int i = 0; i < count; i++) {
+            num1[i] * num2[i];
+        }
+        sc.close();
+    }
+}
