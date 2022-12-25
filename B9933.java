72650a3f59ab01945674f99b7157d841538f046e
2winyear <lsy2246@naver.com>
2022-12-25 14:59:28 +0900

    12/25

--- B9933.java
+++ B9933.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Main {
+public class B9933 {
     static public void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
